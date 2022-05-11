79e8b761e0c3ac96d1c59caa142ff37e510915dc
NatGames <salavat-iskhakov@mail.ru>
2022-05-11 13:01:45 +0300

    run build

---
+++ build/precache-manifest.eca1e2d42b1e2474cfc73344ba8e9437.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab18352b0e8272dacab5eee72bc14acd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a3853bacfffa5bdf1b7",
+    "url": "/static/js/2.c09724f2.chunk.js"
+  },
+  {
+    "revision": "9ba2edfdb64b7002656d",
+    "url": "/static/js/main.95e3d0e9.chunk.js"
+  },
+  {
+    "revision": "fc6ad3c7e4f64a228dc4",
+    "url": "/static/js/runtime-main.9571bbe3.js"
+  }
+]);
